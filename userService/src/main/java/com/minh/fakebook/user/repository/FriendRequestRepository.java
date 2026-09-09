047336fd404da8bf94b63980f4e173e1f6118937
MinhElly <minhnguyen071006@gmail.com>
2026-09-09 16:22:05 +0700

    fix(friend-request): add authorize ADMIN for CRUD endpoint, create new endpoint for authorized User

--- userService/src/main/java/com/minh/fakebook/user/repository/FriendRequestRepository.java
+++ userService/src/main/java/com/minh/fakebook/user/repository/FriendRequestRepository.java
@@ -3,6 +3,9 @@ package com.minh.fakebook.user.repository;
 import com.minh.fakebook.user.domain.FriendRequest;
 import com.minh.fakebook.user.domain.enumeration.FriendRequestStatus;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +17,10 @@ import org.springframework.stereotype.Repository;
 public interface FriendRequestRepository
         extends JpaRepository<FriendRequest, UUID>, JpaSpecificationExecutor<FriendRequest> {
     boolean existsBySenderIdAndReceiverIdAndStatus(UUID senderId, UUID receiverId, FriendRequestStatus status);
-    java.util.Optional<FriendRequest> findBySenderIdAndReceiverIdAndStatus(UUID senderId, UUID receiverId, FriendRequestStatus status);
+
+    java.util.Optional<FriendRequest> findBySenderIdAndReceiverIdAndStatus(UUID senderId, UUID receiverId,
+            FriendRequestStatus status);
+
+    Page<FriendRequest> findByReceiverIdAndStatus(UUID receiverId, FriendRequestStatus status, Pageable pageable);
+    Page<FriendRequest> findBySenderIdAndStatus(UUID senderId, FriendRequestStatus status, Pageable pageable);
 }
