fae8164815f1798b8716dc5e20570c284cbb6839
MinhElly <minhnguyen071006@gmail.com>
2026-09-15 09:33:53 +0700

    feat(user): add feign media client, friendship check API and kafka event publishers

---
+++ userService/src/main/java/com/minh/fakebook/user/config/FeignUserRelayRequestInterceptor.java
@@ -0,0 +1,23 @@
+package com.minh.fakebook.user.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+@Component 
+public class FeignUserRelayRequestInterceptor implements RequestInterceptor{
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_TOKEN_TYPE = "Bearer";
+
+    public void apply(RequestTemplate template){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication instanceof JwtAuthenticationToken jwtAuthenticationToken){
+            String tokenValue = jwtAuthenticationToken.getToken().getTokenValue();
+            template.header(AUTHORIZATION_HEADER, String.format("%s %s", BEARER_TOKEN_TYPE, tokenValue));
+        }
+    }
+}
