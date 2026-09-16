40c71b164b156ffeb03569d08a9e1b9d1d3262ab
MinhElly <minhnguyen071006@gmail.com>
2026-09-16 17:37:04 +0700

    fix(feed-service): add annotation for consumner

--- feedService/src/main/java/com/minh/fakebook/feed/broker/PostEventListener.java
+++ feedService/src/main/java/com/minh/fakebook/feed/broker/PostEventListener.java
@@ -6,12 +6,14 @@ import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.minh.fakebook.feed.service.FeedFanoutService;
 import com.minh.fakebook.feed.service.event.PostCreatedEvent;
 
+@Component("processPostEvent")
 public class PostEventListener implements Consumer<String>{
     private static final Logger LOG = LoggerFactory.getLogger(PostEventListener.class);
     
