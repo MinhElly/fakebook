05959b834bdd887b61b6dc86c9e47d32b1ab66c4
MinhElly <minhnguyen071006@gmail.com>
2026-09-17 09:57:36 +0700

    refactor(feign): standardize feign client target service names to lowercase (TASK-09)

--- userService/src/main/java/com/minh/fakebook/user/client/MediaServiceClient.java
+++ userService/src/main/java/com/minh/fakebook/user/client/MediaServiceClient.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-@FeignClient (name = "mediaService", path = "/api/media", fallback = MediaServiceClientFallback.class)
+@FeignClient (name = "mediaservice", path = "/api/media", fallback = MediaServiceClientFallback.class)
 public interface MediaServiceClient {
     @GetMapping ("/{id}")
     MediaDTO getMediaById(@PathVariable ("id") UUID id);
